1c7ac261805c41cba96908b83e3621d1b5de4a73
meta-bay <bayiemeseret@gmail.com>
2024-07-24 21:51:03 +0300

    basic cashing

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -14,10 +14,12 @@ class BasicCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
+        '''puts the key and value in the dictionary'''
         if key is not None or item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        '''retreives the value'''
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
