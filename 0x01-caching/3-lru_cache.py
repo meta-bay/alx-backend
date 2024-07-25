6787f2267b571de37fb5119fad874a7057b427ae
meta-bay <bayiemeseret@gmail.com>
2024-07-25 05:06:58 +0300

    lru algorithm

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -3,6 +3,7 @@
 LRU caching module
 '''
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class LRUCache(BaseCaching):
@@ -10,21 +11,30 @@ class LRUCache(BaseCaching):
     def __init__(self):
         '''Initialize'''
         super().__init__()
-        
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         '''Creates dictionary in LRU algorithm'''
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            keys = []
-            for key, _ in self.cache_data.items():
-                keys.append(key)
-            print(f"DISCARD {keys[0]}")
-            del self.cache_data[keys[0]]
-        if key is not None or item is not None:
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            # Remove the old value to update the order
+            self.cache_data.pop(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # Pop the first item (least recently used)
+            discarded_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {discarded_key}")
+
+        # Insert the item as the most recently used
+        self.cache_data[key] = item
 
     def get(self, key):
         '''retreives the dictionary by key'''
         if key is None or key not in self.cache_data:
             return None
-        return self.cache_data[key]
+
+        # Move the accessed item to the end to mark it as most recently used
+        value = self.cache_data.pop(key)
+        self.cache_data[key] = value
+        return value
