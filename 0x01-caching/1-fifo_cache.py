c70c5dc2f5ec88ffaacdf6634e7b764538fb3c5e
meta-bay <bayiemeseret@gmail.com>
2024-07-25 04:44:44 +0300

    does this work

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,33 +1,41 @@
 #!/usr/bin/env python3
-'''
-FIFO caching module
-'''
+"""
+BaseCaching module
+"""
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    '''FIFO caching system class'''
+    """
+    FIFOCache defines a FIFO caching system
+    """
 
     def __init__(self):
-        '''Initialize'''
+        """
+        Initialize the class with the parent's init method
+        """
         super().__init__()
-        self.key_order = []
+        self.order = []
 
     def put(self, key, item):
-        '''Creates dictionary in FIFO algorithm'''
+        """
+        Cache a key-value pair
+        """
         if key is None or item is None:
             pass
         else:
             length = len(self.cache_data)
             if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD {}".format(self.key_order[0]))
-                del self.cache_data[self.key_order[0]]
-                del self.key_order[0]
-            self.key_order.append(key)
+                print("DISCARD: {}".format(self.order[0]))
+                del self.cache_data[self.order[0]]
+                del self.order[0]
+            self.order.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
-        '''retreives the dictionary by key'''
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        """
+        Return the value linked to a given key, or None
+        """
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
