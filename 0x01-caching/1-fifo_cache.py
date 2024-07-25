e4b4401ebbd3349586a760fc9385327e1c3b4960
meta-bay <bayiemeseret@gmail.com>
2024-07-25 04:48:37 +0300

    just work

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -28,6 +28,6 @@ class FIFOCache(BaseCaching):
 
     def get(self, key):
         '''retreives the dictionary by key'''
-        if key is None or key not in self.cache_data:
+        if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
