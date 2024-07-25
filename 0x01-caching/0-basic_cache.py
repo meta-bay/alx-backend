ab9cb6c4113acc6ce75a7ffcbe4fc24bdec753df
meta-bay <bayiemeseret@gmail.com>
2024-07-25 04:02:29 +0300

    fix code

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -20,6 +20,6 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         '''retreives the value'''
-        if key is None or key not in self.cache_data.items():
+        if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
