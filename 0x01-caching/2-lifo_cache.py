82b4974d6651c156acdae3be76c47145cbb38c2f
meta-bay <bayiemeseret@gmail.com>
2024-07-25 04:55:24 +0300

    lifo algorithm

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -12,17 +12,20 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """Initialize"""
         super().__init__()
+        self.key_order = []
 
     def put(self, key, item):
         '''creates dictionary in LIFO algorithm'''
-        if key is not None or item is not None:
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.key_order[-1]))
+                del self.cache_data[self.key_order[-1]]
+                del self.key_order[-1]
+            self.key_order.append(key)
             self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            keys = []
-            for k, _ in self.cache_data.items():
-                keys.append(k)
-            print(f"DISCARD {keys[-1]}")
-            del self.cache_data[keys[-1]]
 
     def get(self, key):
         '''retreives the dictionary by key'''
