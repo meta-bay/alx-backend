ad0e4f29ab9d27de97cc90fec118b842b11301a3
meta-bay <bayiemeseret@gmail.com>
2024-07-25 04:18:30 +0300

    fix code

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -20,6 +20,6 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         '''retreives the value'''
-        if key is None or key not in self.cache_data:
+        if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -10,17 +10,18 @@ class LRUCache(BaseCaching):
     def __init__(self):
         '''Initialize'''
         super().__init__()
+        
 
     def put(self, key, item):
         '''Creates dictionary in LRU algorithm'''
-        if key is not None or item is not None:
-            self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             keys = []
             for key, _ in self.cache_data.items():
                 keys.append(key)
             print(f"DISCARD {keys[0]}")
             del self.cache_data[keys[0]]
+        if key is not None or item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
         '''retreives the dictionary by key'''
