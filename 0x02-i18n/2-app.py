81d0e820d6921cd341a69669994595e198fc0e4a
meta-bay <bayiemeseret@gmail.com>
2024-07-31 05:23:13 +0300

    get locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''
+1-app module
+'''
+from flask import Flask, render_template
+from flask_babel import Babel
+from typing import List
+from flask import request
+
+
+class Config:
+    '''config class'''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index() -> str:
+    '''returns the index'''
+    return render_template('2-index.html')
