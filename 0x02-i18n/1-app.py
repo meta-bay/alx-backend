b349cb8b01030f946a73c1b6d4f50f8ed38f4b0f
meta-bay <bayiemeseret@gmail.com>
2024-07-31 05:03:55 +0300

    basic babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,12 +7,14 @@ from flask_babel import Babel
 from typing import List
 
 app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 babel = Babel(app)
 
 
 class Config:
     '''config class'''
-    LANGUAGES: List[str, str] = ['en', 'fr']
+    LANGUAGES: List[str] = ['en', 'fr']
 
 
 @app.route('/')
