3bf33fdd750d34b14c210a775d18648bf039615b
meta-bay <bayiemeseret@gmail.com>
2024-07-31 05:25:40 +0300

    get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -21,5 +21,6 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
+    '''get locale from request'''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
